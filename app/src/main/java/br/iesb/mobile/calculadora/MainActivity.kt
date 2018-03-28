4c9aef600dcd1980ab1d2410b28f7d839b3b028c
evertonnogueira <evertonnp10@gmail.com>
2018-03-28 00:02:24 -0300

    Criando o projeto da calculadora no github

---
+++ app/src/main/java/br/iesb/mobile/calculadora/MainActivity.kt
@@ -0,0 +1,20 @@
+package br.iesb.mobile.calculadora
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private textView: TextView;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickButton(view: View){
+        R.id.textUm
+    }
+}
